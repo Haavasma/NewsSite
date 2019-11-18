74cec2327f7b334fbe750ccac7f3afa69b8a549e
haavasma <haavasma@stud.ntnu.no>
2019-11-18 16:24:20 +0100

    added test for deleting article

--- server/src/dao/sakdao.test.js
+++ server/src/dao/sakdao.test.js
@@ -27,32 +27,42 @@ afterAll(() => {
   pool.end();
 });
 
-test("get one Sak from db", done =>{
-    function callback(status, data){
-        console.log(status, data);
-        expect(data.length).toBe(1);
-        expect(data[0].brukernavn).toBe("Zanacion");
-        done();
-    }
-    sakDao.getNyhet(1, callback);
+test("get one Sak from db", done => {
+  function callback(status, data) {
+    console.log(status, data);
+    expect(data.length).toBe(1);
+    expect(data[0].brukernavn).toBe("Zanacion");
+    done();
+  }
+  sakDao.getNyhet(1, callback);
 });
 
-test("get every Sak from db", done =>{
-  function callback(status, data){
+test("get every Sak from db", done => {
+  function callback(status, data) {
     console.log(status, data);
     expect(data.length).toBe(2);
     expect(data[0].brukernavn).toBe("Zanacion");
     expect(data[0].overskrift).toBe("overskrift");
     expect(data[0].innhold).toBe("dette er et innhold");
+    done();
   }
   sakDao.getNyheter(callback);
 });
 
-test("get brukernavn with sak_id", done=>{
-function callback(status, data){
-  console.log(status, data);
-  expect(data.length).toBe(1);
-  expect(data[0].brukernavn).toBe("Zanacion");
-}
-sakDao.getBrukernavnAvSak_id(1, callback);
+test("get brukernavn with sak_id", done => {
+  function callback(status, data) {
+    console.log(status, data);
+    expect(data.length).toBe(1);
+    expect(data[0].brukernavn).toBe("Zanacion");
+    done();
+  }
+  sakDao.getBrukernavnAvSak_id(1, callback);
+});
+test("slett nyhetsSak", done =>{
+  function callback(status, data){
+    console.log(status, data);
+    expect(data.length).toBe(1);
+    done();
+  }
+  sakDao.deleteNyhet(1, callback);
 });
