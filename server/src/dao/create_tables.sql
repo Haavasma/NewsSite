04f398356b68f2f88f2560a9ece3f6638463aedf
haavasma <haavasma@stud.ntnu.no>
2019-11-18 20:55:40 +0100

    asd

--- server/src/dao/create_tables.sql
+++ server/src/dao/create_tables.sql
@@ -41,7 +41,7 @@ CREATE TABLE `kommentar` (
  `sak_id` int(11) DEFAULT NULL,
  PRIMARY KEY (`kommentar_id`),
  KEY `sak_id` (`sak_id`),
- CONSTRAINT `kommentar_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE, ON UPDATE CASCADE
+ CONSTRAINT `kommentar_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 
 
@@ -52,7 +52,7 @@ CREATE TABLE `rating` (
  PRIMARY KEY (`sak_id`,`brukernavn`),
  KEY `sak_id` (`sak_id`),
  KEY `brukernavn` (`brukernavn`),
- CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE,
+ CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE ON UPDATE CASCADE,
  CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`brukernavn`) REFERENCES `bruker` (`brukernavn`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
