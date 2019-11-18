260333685ec1a9118d890c7a3761605071f82d17
haavasma <haavasma@stud.ntnu.no>
2019-11-18 20:59:21 +0100

    f

--- server/src/dao/create_tables.sql
+++ server/src/dao/create_tables.sql
@@ -30,8 +30,8 @@ CREATE TABLE `sak` (
  PRIMARY KEY (`sak_id`),
  KEY `kategori` (`kategori`),
  KEY `brukernavn` (`brukernavn`),
- CONSTRAINT `sak_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
- CONSTRAINT `sak_ibfk_2` FOREIGN KEY (`brukernavn`) REFERENCES `bruker` (`brukernavn`) ON DELETE CASCADE ON UPDATE CASCADE
+ CONSTRAINT `sak_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kategori`) ON DELETE CASCADE,
+ CONSTRAINT `sak_ibfk_2` FOREIGN KEY (`brukernavn`) REFERENCES `bruker` (`brukernavn`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `kommentar` (
@@ -41,7 +41,7 @@ CREATE TABLE `kommentar` (
  `sak_id` int(11) DEFAULT NULL,
  PRIMARY KEY (`kommentar_id`),
  KEY `sak_id` (`sak_id`),
- CONSTRAINT `kommentar_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE ON UPDATE CASCADE
+ CONSTRAINT `kommentar_ibfk_1` FOREIGN KEY (`sak_id`) REFERENCES `sak` (`sak_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 
 
