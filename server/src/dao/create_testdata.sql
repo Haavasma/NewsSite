695080b01b037c7f2148ad5c6cd4ba7febed2955
haavasma <haavasma@stud.ntnu.no>
2019-11-18 04:02:07 +0100

    actually fixed sql syntax

--- server/src/dao/create_testdata.sql
+++ server/src/dao/create_testdata.sql
@@ -1,20 +1,20 @@
-INSERT INTO TABLE kategori(kategori) values("Sport");
-INSERT INTO TABLE kategori(kategori) values("Politikk");
+INSERT INTO kategori(kategori) values("Sport");
+INSERT INTO kategori(kategori) values("Politikk");
 
-INSERT INTO TABLE bruker(brukernavn, passord) values("Zanacion", "secret");
+INSERT INTO bruker(brukernavn, passord) values("Zanacion", "secret");
 
-INSERT INTO TABLE sak values(DEFAULT,"Zanacion", "overskrift", "dette er et innhold", "04/11/2019 @18:00", 
+INSERT INTO sak values(DEFAULT,"Zanacion", "overskrift", "dette er et innhold", "04/11/2019 @18:00", 
 "https://cdn.ymaws.com/www.itsmfusa.org/resource/resmgr/images/more_images/news-3.jpg",
 "Sport",
 1
 );
-INSERT INTO TABLE sak values(DEFAULT, "Zanacion", "overskrift2", "dette er et innhold2", "04/11/2019 @18:00", 
+INSERT INTO sak values(DEFAULT, "Zanacion", "overskrift2", "dette er et innhold2", "04/11/2019 @18:00", 
 "https://cdn.ymaws.com/www.itsmfusa.org/resource/resmgr/images/more_images/news-3.jpg",
 "Sport",
 0
 );
-INSERT INTO TABLE kommentar values(DEFAULT, "bra aritkkel", "harald", 1);
-INSERT INTO TABLE kommentar values(DEFAULT, "dårlig aritkkel", "harald", 2);
+INSERT INTO kommentar values(DEFAULT, "bra aritkkel", "harald", 1);
+INSERT INTO kommentar values(DEFAULT, "dårlig aritkkel", "harald", 2);
 
-INSERT INTO TABLE rating values(3, 1, "Zanacion");
-INSERT INTO TABLE rating values(DEFAULT, 2, 1);
+INSERT INTO rating values(3, 1, "Zanacion");
+INSERT INTO rating values(DEFAULT, 2, 1);
